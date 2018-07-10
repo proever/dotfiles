9c1a1f657dbe6b3f54103e5677a2d515291f59e4
Paul Roever <paul@roever-family.net>
2018-07-09 21:30:59 -0700

    adds vi mode to zshrc

--- zshrc.sh
+++ zshrc.sh
@@ -69,6 +69,30 @@ alias f="fuck"
 alias r="source ~/.zsh_profile"
 alias mkdir="mkdir -pv"
 
+bindkey -M menuselect '^[[Z' reverse-menu-complete
+
+bindkey -v
+
+bindkey '^P' up-history
+bindkey '^N' down-history
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+bindkey '^r' history-incremental-search-backward
+
+precmd() { RPROMPT="" }
+function zle-line-init zle-keymap-select {
+   VIM_PROMPT_NORMAL="%{$fg_bold[magenta]%} [%  NORMAL ]% %{$reset_color%}"
+   VIM_PROMPT_INSERT="%{$fg_bold[green]%} [%  INSERT ]% %{$reset_color%}"
+   RPS1="${${KEYMAP/vicmd/$VIM_PROMPT_NORMAL}/(main|viins)/$VIM_PROMPT_INSERT} $EPS1"
+   zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+export KEYTIMEOUT=1
+
 fpath=(/usr/local/share/zsh-completions $fpath)
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
