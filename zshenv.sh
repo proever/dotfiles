b4e84ca524d3e4959812e686b0f0adcf2678c7cc
Paul Roever <paul@roever-family.net>
2018-07-11 16:51:08 -0700

    adds FZF options and default commands that use fd

--- zshenv.sh
+++ zshenv.sh
@@ -2,6 +2,10 @@ export EDITOR='nvim'
 
 export FPP_EDITOR='nvim'
 
+export FZF_DEFAULT_COMMAND='fd . --type=file'
+export FZF_CTRL_T_COMMAND='fd . -H $HOME --type=file'
+export FZF_ALT_C_COMMAND='fd . -H --type=directory'
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 export FZF_COMPLETION_TRIGGER='ff'
 
 export PYENV_ROOT="$HOME/.pyenv"
