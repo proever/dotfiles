5f0d1aece181d367bbd31dea8b7ddf6ac20eadcd
Test <test@Tests-Mac.local>
2018-07-14 19:58:01 -0700

    updated install.sh
    - removed unnecessary zsh installation steps, oh-my-zsh handles this
    - removed xquartz from cask installation
    - added more symlinks, but removed zshrc, since oh-my-zsh handles this

--- install.sh
+++ install.sh
@@ -3,12 +3,17 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install emacs fd fzf neovim nvm pyenv pyenv-virtualenv rbenv the_silver_searcher thefuck tmux tree zsh zsh-completions
 /usr/local/opt/fzf/install
-compaudit | xargs chmod g-w
-sudo cat "/usr/local/bin/zsh" /etc/shells
-chsh -s $(which zsh)
-brew cask install emacs iterm2 slack spectacle sublime-text xquartz
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mkdir Developer
-git clone https://github.com/proever/dotfiles.git Developer
-ln -s ~/Developer/dotfiles/zshrc.sh ~/.zshrc
+
+mkdir -p ~/.config/nvim
+mkdir ~/.emacs.d
+
+brew cask install emacs iterm2 slack spectacle sublime-text
 ln -s ~/Developer/dotfiles/zshenv.sh ~/.zshenv
+ln -s ~/Developer/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/Developer/dotfiles/spacemacs.el ~/.spacemacs
+ln -s ~/Developer/dotfiles/profile.sh ~/.profile
+ln -s ~/Developer/dotfiles/pythonrc.py ~/.pythonrc
+ln -s ~/Developer/dotfiles/init.el ~/.emacs.d/init.el
+ln -s ~/Developer/dotfiles/init.vim ~/.config/nvim/init.vim
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
